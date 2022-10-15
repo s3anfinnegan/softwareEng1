9938c71777eb36929cce9db06b4ea81f5c4f4964
sean <sean2001finnegan@gmail.com>
2022-10-15 14:06:17 +0100

    modules update

--- app/src/main/java/software/eng/assignment/app/Module.java
+++ app/src/main/java/software/eng/assignment/app/Module.java
@@ -1,4 +1,4 @@
-package org.example;
+/**package org.example;
 import java.util.ArrayList;
 
 public class Module {
@@ -39,5 +39,56 @@ public class Module {
     public Lecturer getLecturer() {
         return lecturer;
     }
+
+}**/
+
+package org.example;
+import java.util.ArrayList;
+
+public class Modules {
+
+    public String modName;
+    public String modID;
+    public ArrayList<Student> studentList;
+    public Lecturer lecturer;
+
+    public Modules(String modName, String modID) {
+        this.modName=modName;
+        this.modID=modID;
+    }
+
+    public String getModName() {
+        return modName;
+    }
+
+    public void setModName(String modName) {
+        this.modName = modName;
+    }
+
+    public String getModID() {
+        return modID;
+    }
+
+    public void setModID(String modID) {
+        this.modID = modID;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
+
 
 }
